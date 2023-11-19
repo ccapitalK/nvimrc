a429c9d6baa33054490f57347d6c8981c8de8c90
Sahan Fernando <sahan.h.fernando@gmail.com>
2023-11-19 19:43:09 +1100

    Add navigation shortcuts

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -28,6 +28,10 @@ lsp.on_attach(function(client, bufnr)
     local opts = {buffer = bufnr, remap = false}
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.declaration() end, opts)
+    vim.keymap.set("n", "<leader>gs", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "<leader>vu", function() vim.lsp.buf.incoming_calls() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
