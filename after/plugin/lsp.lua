446e5b264e4ec29491930e0cf1e1cbcfdec3e805
Sahan Fernando <sahan.h.fernando@gmail.com>
2023-12-27 19:48:24 +1100

    Fix cmp bindings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -14,8 +14,8 @@ local cmp = require('cmp')
 local cmp_select = {behaviour = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-p>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-p>'] = cmp.mapping.confirm({select = true}),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({select = true}),
     ['<C-Space>'] = cmp.mapping.complete()
 })
 
