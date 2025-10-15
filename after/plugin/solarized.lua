af22072ec84b9aac865ac87b83d1fd564fd38bb1
Sahan Fernando <sahan.h.fernando@gmail.com>
2025-10-16 04:08:48 +1100

    Better solarized theming for folds

--- after/plugin/solarized.lua
+++ after/plugin/solarized.lua
@@ -1,3 +1,4 @@
 -- Transparent background
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "Folded", { bg = nil, fg = nil })
