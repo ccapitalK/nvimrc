0906cc0f5ebe60805549bc5f09c35c51c2c79071
Sahan Fernando <sahan.h.fernando@gmail.com>
2026-02-12 23:38:12 +1100

    Make comments green

--- after/plugin/solarized.lua
+++ after/plugin/solarized.lua
@@ -2,3 +2,4 @@
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "Folded", { bg = nil, fg = nil })
+vim.api.nvim_set_hl(0, "Comment", { fg = "#859900", italic = true })
